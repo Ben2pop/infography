7a260760f844a2575d6903b89a22655ec688894f
ben2pop <r.bendenoun@gmail.com>
2013-07-14 23:19:47 +0200

    first commit & bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infography::Application.config.secret_token = '7b7eef73874a2e38000c71ef18d6c8b590f66e8b166664bb7b54039ee69520a5c6a3e6ed34ca44dd78ff39eadedb08feb492aa4b8b85f33b0fdab8e870a58384'
